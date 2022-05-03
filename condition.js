cd02460aff889fca129894dda446d1f4d5f5e3da
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-03 19:48:44 +0600

    a simply condition added

---
+++ condition.js
@@ -0,0 +1,17 @@
+/*------------------------- condition -----------------------*/
+
+var isHot = false;
+
+if (isHot != true) {
+    greetings = 'This is a good day';
+    console.log(greetings);
+}
+else {
+    badDay = 'This is a very Bad day';
+    console.log(badDay);
+}
+
+
+
+
+
