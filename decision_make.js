8b8466fea6d36f3a9fc1dcfffe965cee2d403f4c
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-05-03 23:29:02 +0600

    price variable added

--- decision_make.js
+++ decision_make.js
@@ -1,2 +1,10 @@
 /*------------------------------ mobile buying decision ---------------------------- */
 
+var samPrice = 500;
+var nokPrice = 400;
+var miPrice = 300;
+var sonyPrice = 600;
+var applePrice = 2000;
+var symPrice = 200;
+
+var haveMoney = 1000;
