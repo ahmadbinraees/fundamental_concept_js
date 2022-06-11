286113731998e1052d224b3e13a13377e268bf0f
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-06-11 11:39:28 +0600

    comment out updated

--- recap.js
+++ recap.js
@@ -1,11 +1,9 @@
-/* 
-1- what is array?
-2- Declare an array;
-3- Array index;
-4- index.of use;
-5- add and remove element from array;
-6- Compare variable and comparison operator;
-7- Conditional decision making through if else;
-8- Handle multiple condition && ||;
-9- Multi stage condition and nested condition;
-*/
+//  what is array?
+//  Declare an array;
+//  Array index;
+//  index.of use;
+//  add and remove element from array;
+//  Compare variable and comparison operator;
+//  Conditional decision making through if else;
+//  Handle multiple condition && ||;
+//  Multi stage condition and nested condition;
