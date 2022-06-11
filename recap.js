7ea0ba4670188bf27f16e0f442a06a72898ea40a
ahmadbinraees <ahmadullahbinraees@gmail.com>
2022-06-11 11:34:12 +0600

    fundamentals recap subject added

---
+++ recap.js
@@ -0,0 +1,11 @@
+/* 
+1- what is array?
+2- Declare an array;
+3- Array index;
+4- index.of use;
+5- add and remove element from array;
+6- Compare variable and comparison operator;
+7- Conditional decision making through if else;
+8- Handle multiple condition && ||;
+9- Multi stage condition and nested condition;
+*/
